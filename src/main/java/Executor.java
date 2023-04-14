22300c8e4887ba8bde28976178fb9b1f67b2889f
Liu Peiyuan <peiyuanix@gmail.com>
2023-04-14 10:17:28 +0800

    update set executor

--- src/main/java/Executor.java
+++ src/main/java/Executor.java
@@ -42,8 +42,8 @@ public class Executor {
 
     private void set(String sql, OperationType opType) throws SQLException {
         @Cleanup Statement stat = this.conn.createStatement();
-        int count = stat.executeUpdate(sql);
-        TraceCollector.collect(new Trace(this.executorId, sql, opType, count));
+        boolean isResultSet = stat.execute(sql);
+        TraceCollector.collect(new Trace(this.executorId, sql, opType, 0));
     }
 
     private void commit() throws SQLException {
